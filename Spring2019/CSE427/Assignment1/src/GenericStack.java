cdbc7e7e14a38225611e814259dbd694892dbb29
Muhammad Sheehab <sheehabx@gmail.com>
2019-03-17 17:01:00 +0600

    empty class with only the function signatures

---
+++ Spring2019/CSE427/Assignment1/src/GenericStack.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class GenericStack<Object>{
+
+    public GenericStack() {
+
+    }
+
+
+    public void push (Object X){
+
+    }
+
+    /*
+    public Object pop (){
+        return Object;
+    }
+    */
+
+    public boolean isEmpty (){
+        return true;
+    }
+
+
+}
