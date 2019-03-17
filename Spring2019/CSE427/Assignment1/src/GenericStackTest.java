a709746fe8bca34231c3256ab42f4b11e32b7404
Muhammad Sheehab <sheehabx@gmail.com>
2019-03-17 17:14:09 +0600

    added Test Functions and Test Class

---
+++ Spring2019/CSE427/Assignment1/src/GenericStackTest.java
@@ -0,0 +1,24 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+class GenericStackTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void push() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void pop() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+    }
+}
