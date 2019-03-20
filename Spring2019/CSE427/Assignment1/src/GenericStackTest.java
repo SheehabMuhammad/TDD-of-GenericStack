6cc71605fdebd69e038d7d43beaadd4622436506
Muhammad Sheehab <sheehabx@gmail.com>
2019-03-20 14:47:24 +0600

    Added comments with explanations

--- Spring2019/CSE427/Assignment1/src/GenericStackTest.java
+++ Spring2019/CSE427/Assignment1/src/GenericStackTest.java
@@ -23,7 +23,19 @@ class GenericStackTest {
         testStack = null;
     }
 
+    /**
+     *  Use of theory is not possible since Theory is dead for JUnit 5
+     */
 
+
+    /**
+     * Parameterized Test for push method. It takes two
+     * variable one is input and the second expected
+     *
+     * @param input
+     * @param expected
+     *
+     */
     @ParameterizedTest
     @MethodSource("providePartitionedValueForPushTest")
     void pushTest(String input, String expected) {
@@ -37,11 +49,10 @@ class GenericStackTest {
     }
 
     /**
-     * Test case using input space by partitioning the variables into block for push.
+     * Test cases using input space by partitioning the variables into block for push.
      * null values or valid values
      *
      */
-
     private static Stream<Arguments> providePartitionedValueForPushTest() {
         return Stream.of(
                 Arguments.of("abc", "abc"),
@@ -51,7 +62,14 @@ class GenericStackTest {
         );
     }
 
-
+    /**
+     * Parameterized Test for pop method. It takes two
+     * variable one is input and the second expected
+     *
+     * @param input
+     * @param expected
+     *
+     */
     @ParameterizedTest
     @MethodSource("providePartitionedValueForPopTest")
     void popTest(String input, String expected) {
@@ -91,9 +109,9 @@ class GenericStackTest {
         assertEquals("No object found in the stack to pop", exception.getMessage());
     }
 
-
-
-
+    /**
+     * Test cases for isEmpty method
+     */
     @Test
     void isEmpty() {
         /**
@@ -103,7 +121,7 @@ class GenericStackTest {
         assertFalse(testStack.isEmpty());
 
         /**
-         * Poping an object to check for empty stack.
+         * Cleaning up the stack check for empty stack.
          */
         testStack.pop();
         assertTrue(testStack.isEmpty());
